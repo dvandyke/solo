1f85b7adfb0d99c70327ca65bfe10b466fceaa8a
Derek Van Dyke <haoled@gmail.com>
2015-03-09 22:32:56 -0700

    Updated database schema and tweaked visuals

--- app.js
+++ app.js
@@ -1,15 +1,28 @@
+var randomProperty = function(obj){
+	var keys = Object.keys(obj);
+	return keys[keys.length *Math.random() <<0] ;
+};
+
 $('#submit').on('click', function(){
+	var param = $('.active').find('input').attr('id').toString();
+	console.log(param);
 	$('#myModal').modal('hide');
 	$('.lead').fadeOut(1000);
-	myFirebaseRef.orderByValue().limitToFirst(1).on('child_added', function(snapshot){
+	myFirebaseRef.child(param).on('value', function(snapshot){
 		$('.cover-heading').fadeOut(1000, function(){
 			console.log(snapshot.val());
-			$('.cover-heading').html(snapshot.val() + ' - '+snapshot.key()
-				+'<span class="glyphicon glyphicon-star"></span>').fadeIn(2000);
+			var author = randomProperty(snapshot.val());
+			console.log(author);
+			$('.cover-heading').html(snapshot.val()[author] + ' <br>- 	<i>'+author+'</i> '
+				+'<span class="glyphicon glyphicon-star-empty"></span>').fadeIn(2300);
 		})
 		// $('.cover-heading').fadeOut(function(){	
 		// });
 	});
 });
 
-var myFirebaseRef = new Firebase('https://sizzling-inferno-4974.firebaseio.com/');
+$('body').on('click', '.glyphicon', function(){
+	$(this).toggleClass('glyphicon-star-empty glyphicon-star');
+})
+
+var myFirebaseRef = new Firebase('https://sizzling-inferno-4974.firebaseio.com/%20Quotes');
